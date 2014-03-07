2745dbad138282c31342da6f6d5ff07a3db8d171
jstonevalley <jonas.stendahl@outlook.com>
2014-03-07 09:55:36 +0100

    Added an exampleview and controller and an app.js

---
+++ js/controller/homeController.js
@@ -0,0 +1,16 @@
+var HomeController = function(view, model){
+
+	view.chooseDishBtn.click(chooseDish);
+	view.backBtn.click(back);
+
+	function chooseDish(){
+		var id = model.getCurrentDishId();
+		model.addDishToMenu(id);
+		back();
+	}
+	function back(){
+		view.confirmBtn.removeAttr("disabled");
+		window.dishPage.hide();
+		window.mainContent.show();
+	}
+}
