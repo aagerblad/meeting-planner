c127d9bb4f7fc21dcfcdb2a553a71a9980b73f02
Jonas Stendahl <jonas.stonevalley@gmail.com>
2014-03-19 11:03:52 +0100

    Placeholders when trying to create activity without filling all fields. Data cleared when creating Activity.

--- js/controller/modalController.js
+++ js/controller/modalController.js
@@ -1,15 +1,41 @@
 var ModalController = function(view, model){
 
-	//view.acForm.submit(addActivity);
-	view.acForm.submit(function(e) {
-    e.preventDefault();
-    addActivity();
-    view.acModal.modal('hide');
-});
+    //view.acForm.submit(addActivity);
+    view.acForm.submit(function(e) {
+        e.preventDefault();
+        if(addActivity())
+            view.acModal.modal('hide');
+    });
 
-	function addActivity() {
-		model.addActivity(new Activity(view.acTitle.val(), view.acTime.val(), view.acTypes.val(), view.acDesc.val(), model.getNextId(), model), null);
-	}
+    view.acModal.on('hidden.bs.modal', clearAllFields);
+
+    function addActivity() {
+        if (view.acTitle.val() != "" && view.acTime.val() != "" && view.acTypes.val() != "" && view.acDesc.val() != ""){
+            model.addActivity(new Activity(view.acTitle.val(), view.acTime.val(), view.acTypes.val(), view.acDesc.val(), model.getNextId(), model), null);
+            clearAllFields();
+            return true;
+        }
+        else{
+            if (view.acTitle.val() == ""){
+                view.acTitle.attr("placeholder", "You have to set a title!");
+            }
+            if (view.acTime.val() == ""){
+                view.acTime.attr("placeholder", "You have to set a duration!");
+            }
+            if (view.acDesc.val() == ""){
+                view.acDesc.attr("placeholder", "You have to set a description!");
+            }
+            return false;
+        }
+    }
+    function clearAllFields(){
+        view.acTitle.val("");
+        view.acTitle.attr("placeholder", "");
+        view.acTime.val("");
+        view.acTime.attr("placeholder", "");
+        view.acDesc.val("");
+        view.acDesc.attr("placeholder", "");
+    }
 
 	
 }
