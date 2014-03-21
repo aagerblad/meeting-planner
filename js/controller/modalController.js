8b53357a061e82586e6076de689ad6fddb0e3ce1
ranby <erik.ranby@gmail.com>
2014-03-21 10:58:40 +0100

    time now must be positive. And small bug fixed

--- js/controller/modalController.js
+++ js/controller/modalController.js
@@ -22,25 +22,13 @@ var ModalController = function(view, model){
         var time = parseInt(view.acTime.val());
 
         var limit = 1000;
-        if (view.acTitle.val() != "" && view.acTime.val() != "" && time < limit && view.acTypes.val() != "" && view.acDesc.val() != ""){
+        if (view.acTitle.val() != "" && view.acTime.val() != "" && time < limit && view.acTypes.val() != "" && time > 0 && view.acDesc.val() != ""){
             model.addActivity(new Activity(view.acTitle.val(), parseInt(view.acTime.val()), view.acTypes.val(), view.acDesc.val(), model.getNextId(), model, null), null);
             clearAllFields();
             return true;
         }
         else{
-            if (view.acTitle.val() == ""){
-                view.acTitle.attr("placeholder", "You have to set a title!");
-            }
-            if (view.acTime.val() == ""){
-                view.acTime.attr("placeholder", "You have to set a duration!");
-            }
-            if (time >= limit){
-                view.acTime.val('');
-                view.acTime.attr("placeholder", time +" min is too long! Below 1000 min is ok.");
-            }
-            if (view.acDesc.val() == ""){
-                view.acDesc.attr("placeholder", "You have to set a description!");
-            }
+            checkFieldsForErrors(limit);
             return false;
         }
     }
@@ -52,11 +40,17 @@ var ModalController = function(view, model){
 
         var activity = model.allActivities[view.acModal.attr("title")];
         var thisTime = activity.getLength();
-        var day = model.days[activity.getDayId()];
+        var day;
+        var limit
+        if(activity.getDayId() ) {
+            day = model.days[activity.getDayId()];
+            limit = (24*60) - day.getEndTime() + thisTime;
+        } else {
+            limit = 1000;
+        }
+        
 
-        var limit = (24*60) - day.getEndTime() + thisTime;
-
-        if (view.acTitle.val() != "" && view.acTime.val() != "" && time < limit && view.acTypes.val() != "" && view.acDesc.val() != ""){
+        if (view.acTitle.val() != "" && view.acTime.val() != "" && time < limit && time > 0 && view.acTypes.val() != "" && view.acDesc.val() != ""){
             activity.setName(view.acTitle.val());
             activity.setLength(parseInt(view.acTime.val()));
             activity.setTypeId(view.acTypes.val());
@@ -66,21 +60,32 @@ var ModalController = function(view, model){
             return true;
         }
         else{
-            if (view.acTitle.val() == ""){
+            checkFieldsForErrors(limit);
+            return false;
+        }
+    }
+
+    // checks which field or fields are not filled in correctly and shows an error message.
+    function checkFieldsForErrors(timeLimit) {
+        var time = parseInt(view.acTime.val());
+
+        if (view.acTitle.val() == ""){
                 view.acTitle.attr("placeholder", "You have to set a title!");
             }
             if (view.acTime.val() == ""){
                 view.acTime.attr("placeholder", "You have to set a duration!");
             }
-            if (time >= limit){
+            if (time >= timeLimit){
                 view.acTime.val('');
-                view.acTime.attr("placeholder", time +" min is too long! Below " + limit + " min is ok.");
+                view.acTime.attr("placeholder", time +" min is too long! Below " + timeLimit + " min is ok.");
+            }
+            if (time <= 0) {
+                view.acTime.val('');
+                view.acTime.attr("placeholder", "Time must be a positive integer");  
             }
             if (view.acDesc.val() == ""){
                 view.acDesc.attr("placeholder", "You have to set a description!");
             }
-            return false;
-        }
     }
 
     // Clear form for next use
